e1526af2bf6a5463b88b4f37338e41e7937bfc54
DatNgo <ngominhdat110115@gmail.com>
2025-03-22 19:19:34 +0700

    persist vector

--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 from rag_pipeline import graph
 
+# Test the application
+print("Testing the application...")
+
 response = graph.invoke({"question": "What is [dbo].[DatabaseLog]?"})
 print(response["answer"])
